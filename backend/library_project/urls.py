9d44426331bbab27d00facd6b1d90ec12ea27293
Ninik14 <n_kanashvili@cu.edu.ge>
2026-09-16 19:37:57 +0400

    Serve media files on Render

--- backend/library_project/urls.py
+++ backend/library_project/urls.py
@@ -18,7 +18,9 @@ from django.contrib import admin
 from django.urls import path,include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
+
 
 
 urlpatterns = [
@@ -30,7 +32,11 @@ urlpatterns = [
 ]
 
 
-urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT
-)
+
+urlpatterns += [
+    re_path(
+        r'^media/(?P<path>.*)$',
+        serve,
+        {'document_root': settings.MEDIA_ROOT},
+    ),
+]
